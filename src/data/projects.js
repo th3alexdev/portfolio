dd42da88eee5cbe057f44b27c5d05ffc56e393f6
AleKs <alexanderfranciscog@gmail.com>
2023-07-23 23:50:15 -0400

    added project data

---
+++ src/data/projects.js
@@ -0,0 +1,35 @@
+const projects = [
+    {
+      title: "Smart Type",
+      description: "A powerful browser extension that allows users to create custom commands to automate repetitive typing tasks.",
+      mockup: "./src/assets/imgs/projects/mockup2.png",
+      repo: "https://github.com/th3alexdev/smart-type/",
+      technologies: ["React", "TypeScript", "JavaScript", "Tailwind"],
+    },
+    {
+      title: "To Do App",
+      description: "",
+      mockup: "./src/assets/imgs/projects/mockup1.png",
+      demo: "https://th3alexdev.github.io/todoapp/",
+      repo: "https://github.com/th3alexdev/todoapp/",
+      technologies: ["JavaScript", "TypeScript", "Sass", "Vitejs"],
+    },
+    {
+    title: "Weather App",
+    description: "A web aplication that allows users to search for a city and retrieve the current weather conditions and the forecast for the week.",
+    mockup: "./src/assets/imgs/projects/mockup3.png",
+    demo: "https://github.com/th3alexdev/weather-app/" ,
+    repo: "https://th3alexdev.github.io/weather-app/",
+    technologies: ["JavaScript", "Sass", "Vitejs", "API Integration"]
+    },
+    {
+    title: "Simon Says",
+    description: "A memory game in which the player must repeat a sequence of colors and sounds randomly throughout the game.",
+    mockup: "./src/assets/imgs/projects/mockup4.png",
+    demo: "https://th3alexdev.github.io/simonsays/" ,
+    repo: "https://github.com/th3alexdev/simonsays",
+    technologies: ["JavaScript", "Sass", "Vitejs"],
+    }
+];
+
+export default projects;
