1a6fd3f82911061e8782c30695249a6537be97d6
AleKs <alexanderfranciscog@gmail.com>
2023-08-03 02:18:24 -0400

    updated project descriptions and mockup paths

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,34 +1,34 @@
 const projects = [
     {
       title: "Smart Type",
-      description: "A powerful browser extension that allows users to create custom commands to automate repetitive typing tasks.",
-      mockup: "./src/assets/imgs/projects/mockup2.png",
+      description: "A powerful browser extension enabling users to create custom commands for automating repetitive typing tasks.",
+      mockup: "/assets/smtype-resized.webp",
       repo: "https://github.com/th3alexdev/smart-type/",
-      technologies: ["React", "TypeScript", "JavaScript", "Tailwind"],
+      technologies: ["React", "TypeScript", "Tailwind"],
     },
     {
       title: "To Do App",
-      description: "",
-      mockup: "./src/assets/imgs/projects/mockup1.png",
+      description: "An interactive task management web application for organizing your daily tasks efficiently.",
+      mockup: "/assets/todoapp-resized.webp",
       demo: "https://th3alexdev.github.io/todoapp/",
       repo: "https://github.com/th3alexdev/todoapp/",
-      technologies: ["JavaScript", "TypeScript", "Sass", "Vitejs"],
+      technologies: ["JavaScript", "Sass", "Vitejs"],
     },
     {
-    title: "Weather App",
-    description: "A web aplication that allows users to search for a city and retrieve the current weather conditions and the forecast for the week.",
-    mockup: "./src/assets/imgs/projects/mockup3.png",
-    demo: "https://github.com/th3alexdev/weather-app/" ,
-    repo: "https://th3alexdev.github.io/weather-app/",
-    technologies: ["JavaScript", "Sass", "Vitejs", "API Integration"]
+      title: "Weather App",
+      description: "A web aplication that allows users to search for a city and retrieve the current weather conditions and the forecast for the week.",
+      mockup: "/assets/weatherapp-resized.webp",
+      demo: "https://th3alexdev.github.io/weather-app/" ,
+      repo: "https://github.com/th3alexdev/weather-app/",
+      technologies: ["JavaScript", "Vitejs", "API's"],
     },
     {
-    title: "Simon Says",
-    description: "A memory game in which the player must repeat a sequence of colors and sounds randomly throughout the game.",
-    mockup: "./src/assets/imgs/projects/mockup4.png",
-    demo: "https://th3alexdev.github.io/simonsays/" ,
-    repo: "https://github.com/th3alexdev/simonsays",
-    technologies: ["JavaScript", "Sass", "Vitejs"],
+      title: "Simon Says",
+      description: "A memory game in which the player must repeat a sequence of colors and sounds randomly throughout the game.",
+      mockup: "/assets/simonsays-resized.webp",
+      demo: "https://th3alexdev.github.io/simonsays/" ,
+      repo: "https://github.com/th3alexdev/simonsays",
+      technologies: ["JavaScript", "Sass", "Vitejs"],
     }
 ];
 
