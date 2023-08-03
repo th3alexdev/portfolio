40663f071cdabfb3d109edc567e8f1c07f878ad4
AleKs <alexanderfranciscog@gmail.com>
2023-08-03 02:19:43 -0400

    implemented translation support

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,30 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { I18nextProvider } from 'react-i18next'
+import i18next from 'i18next'
+import global_es from './translations/es/global.json'
+import global_en from './translations/en/global.json'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const userLanguage = navigator.language.split('-')[0].toUpperCase();
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: userLanguage, 
+  resources: {
+    ES: {
+      global: global_es
+    },
+    EN: {
+      global: global_en
+    }
+  }
+})
 
 root.render(
-    <>
+    <I18nextProvider i18n={i18next}>
       <App />
-    </>
+    </I18nextProvider>
 )
