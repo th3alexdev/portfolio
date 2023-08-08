b6e7c27f7d1c2ee215d7a39214a4dd05bfeada01
AleKs <alexanderfranciscog@gmail.com>
2023-08-08 18:06:38 -0400

    added new files

---
+++ src/data/projects.js
@@ -0,0 +1,35 @@
+const projects = [
+    {
+      title: "Smart Type",
+      description: "A powerful browser extension enabling users to create custom commands for automating repetitive typing tasks.",
+      mockup: "/assets/smtype-resized.webp",
+      repo: "https://github.com/th3alexdev/smart-type/",
+      technologies: ["React", "TypeScript", "Tailwind"],
+    },
+    {
+      title: "To Do App",
+      description: "An interactive task management web application for organizing your daily tasks efficiently.",
+      mockup: "/assets/todoapp-resized.webp",
+      demo: "https://th3alexdev.github.io/todoapp/",
+      repo: "https://github.com/th3alexdev/todoapp/",
+      technologies: ["JavaScript", "Sass", "Vitejs"],
+    },
+    {
+      title: "Weather App",
+      description: "A web aplication that allows users to search for a city and retrieve the current weather conditions and the forecast for the week.",
+      mockup: "/assets/weatherapp-resized.webp",
+      demo: "https://th3alexdev.github.io/weather-app/" ,
+      repo: "https://github.com/th3alexdev/weather-app/",
+      technologies: ["JavaScript", "Vitejs", "API's"],
+    },
+    {
+      title: "Simon Says",
+      description: "A memory game in which the player must repeat a sequence of colors and sounds randomly throughout the game.",
+      mockup: "/assets/simonsays-resized.webp",
+      demo: "https://th3alexdev.github.io/simonsays/" ,
+      repo: "https://github.com/th3alexdev/simonsays",
+      technologies: ["JavaScript", "Sass", "Vitejs"],
+    }
+];
+
+export default projects;
