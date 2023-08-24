1b77853de2635a4d7107411aef877b06bac7ee0c
AleKs <alexanderfranciscog@gmail.com>
2023-08-23 20:46:49 -0400

    Refcator and update data structure

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,52 +1,60 @@
 const movies = [
-    {
-    title: 'The Truman Show',
-    year: '1998',
-    director: 'Peter Weir',
-    img: '/movies/the_trumanshow.webp'
-    },
-    {
-    title: 'Interstellar',
-    year: '2014',
-    director: 'Christopher Nolan',
-    img: '/movies/interstellar.webp'
-    },
-    {
-    title: 'Super Mario Bros',
-    year: '2023',
-    director: 'Aaron Hovarth, Michael Jelenic',
-    img: '/movies/the_super_mario_bros_movie.webp'
-    },
-    {
-    title: 'Rocky I',
-    year: '1976',
-    director: 'Sylvester Stallone ',
-    img: '/movies/rocky_i.webp'
-    },
-    {
-    title: 'Scarface',
-    year: '1983',
-    director: 'Brian De Palma',
-    img: '/movies/scarface.webp'
-    }, 
-    {
-    title: 'Blade Runner 2049',
-    year: '2017',
-    director: 'Denis Villeneuve',
-    img: '/movies/blade_runner_2049.webp'
-    },
-    {
-    title: 'Fight Club',
-    year: '1999',
-    director: 'David Fincher',
-    img: '/movies/fight_club.webp'
-    },
-    {
-    title: 'Klaus',
-    year: '2019',
-    director: 'Sergio Pablos',
-    img: '/movies/klaus.webp'
-    }
+  {
+    id: 0,
+    title: "The Truman Show",
+    year: "1998",
+    director: "Peter Weir",
+    img: "/movies/the_trumanshow.webp",
+  },
+  {
+    id: 1,
+    title: "Interstellar",
+    year: "2014",
+    director: "Christopher Nolan",
+    img: "/movies/interstellar.webp",
+  },
+  {
+    id: 2,
+    title: "Super Mario Bros",
+    year: "2023",
+    director: "Aaron Hovarth, Michael Jelenic",
+    img: "/movies/the_super_mario_bros_movie.webp",
+  },
+  {
+    id: 3,
+    title: "Rocky I",
+    year: "1976",
+    director: "Sylvester Stallone ",
+    img: "/movies/rocky_i.webp",
+  },
+  {
+    id: 4,
+    title: "Scarface",
+    year: "1983",
+    director: "Brian De Palma",
+    img: "/movies/scarface.webp",
+  },
+  {
+    id: 5,
+    title: "Blade Runner 2049",
+    year: "2017",
+    director: "Denis Villeneuve",
+    img: "/movies/blade_runner_2049.webp",
+  },
+  {
+    id: 6,
+    title: "Fight Club",
+    year: "1999",
+    director: "David Fincher",
+    img: "/movies/fight_club.webp",
+  },
+  {
+    id: 7,
+    title: "Klaus",
+    year: "2019",
+    director: "Sergio Pablos",
+    img: "/movies/klaus.webp",
+  },
 ];
 
 export default movies;
